7ec400540a2397e75c303c729c2ac4adb461a3fc
Filipe <162770790+filoroch@users.noreply.github.com>
2025-02-25 18:30:40 +0000

    feat(1001) add solving exercise 1001 in java lang
    Informações sobre o exercicio no readme
    
    Signed-off-by: Filipe <162770790+filoroch@users.noreply.github.com>

---
+++ extremelyBasic/ExtremelyBasic.java
@@ -0,0 +1,16 @@
+package extremelyBasic;
+
+import java.util.Scanner;
+
+public class ExtremelyBasic {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        final int A = input.nextInt();
+        final int B = input.nextInt();
+
+        final int X = A + B;
+        
+        System.out.println("X = " + X);
+    }
+}
---
+++ extremelyBasic/README.md
@@ -0,0 +1,6 @@
+# 1001 - ExtremelyBasic
+Leia duas variaveis A e B e some os valores mostrando o resultado em X. 
+
+| Entrada | Saida | 
+| --- | --- | 
+| 10 <br> 9 |  X = 19 | 
